4f6873def6ece1dc834febb54741c31c5926f3a9
Juansero29 <juansero29@gmail.com>
2017-03-04 22:44:34 +0100

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Juansero29 <juansero29@gmail.com>
    
    # Conflicts:
    #	TP6/EX2/.idea/workspace.xml
    #	TP6/EX2/Contact.java
    #	TP6/EX2/Telephone.java

--- TP7(Farwest)/src/personnages/Sheriff.java
+++ TP7(Farwest)/src/personnages/Sheriff.java
@@ -1,31 +1,31 @@
 package personnages;
 
-public class Sheriff extends Cowboy{
+public class Sheriff extends Cowboy {
 
     private static final String ATTITUDE = "honnête";
     private int nbBrigandsCoffres;
 
-    public Sheriff(String nom, String boisson){
-	super(nom, boisson, ATTITUDE);
+    public Sheriff(String nom, String boisson) {
+        super(nom, boisson, ATTITUDE);
     }
 
-    public Sherrif(String nom){
-	this(nom, "vin");
+    public Sheriff(String nom){
+        this(nom, "Vin");
     }
 
-    public void coffrer(Brigand brigand){
-	parler("Au nom de la loi, je vous arrète" + brigand.getNom() + "!");
-	brigand.seFaireEmprisoner(this);
+    public void coffrer(Brigand brigand) {
+        parler("Au nom de la loi, je vous arrète" + brigand.getNom() + "!");
+        brigand.seFaireEmprisonner(this);
     }
 
-    public void rechercherUnBrigand(Brigand brig){
-        System.out.println("OYEZ OYEZ BRAVE GENS!! " + brig.getMiseAPrix +
-			   "$ à qui arrêtera " + brig.getNom() + " mort ou vif!!");
+    public void rechercherUnBrigand(Brigand brig) {
+        System.out.println("OYEZ OYEZ BRAVE GENS!! " + brig.getMiseAPrix() +
+                "$ à qui arrêtera " + brig.getNom() + " mort ou vif!!");
     }
 
     @Override
-    public void sePresenter(){
-	super.sePresenter();
-	parler("J'ai coffré " + nbBrigandsCoffres + " brigands.");
+    public void sePresenter() {
+        super.sePresenter();
+        parler("J'ai coffré " + nbBrigandsCoffres + " brigands.");
     }
 }
--- TP7(Farwest)/src/prog/Main.java
+++ TP7(Farwest)/src/prog/Main.java
@@ -1,42 +1,65 @@
-package application;
+package prog;
 
 import personnages.*;
 
-public class Main{
+public class Main {
 
-    public static void main(String[] yolo){
+    public static void main(String[] yolo) {
 
-	Humain vilain = new Brigand("Wario","martini","désagréable", 1200f);
-	Humain mario = new Cowboy("Mario", "froggie", "remarquable");
-	Dame princesse = new Dame("Peach");
+        Humain vilain = new Brigand("Wario", "martini", "désagréable", 1200f);
+        Humain mario = new Cowboy("Mario", "froggie", "remarquable");
+        Dame princesse = new Dame("Peach");
 
 
-	mario.sePresenter();
-	princesse.sePresenter();
-	
-	vilain.parler("Je suis vraiment méchant.");
-	
-	((Brigand)vilain).kidnapper(princesse);
-	mario.parler("PAS DE RESPECT RAMENES MA MEUF");
-	vilain.parler("MUAHAHAHAHAHAAA");
-	vilain.sePresenter();
-	mario.parler("Attends, je mange ce champignon et tu vas moins rigoler !");
+        mario.sePresenter();
+        princesse.sePresenter();
 
-	((Cowboy)mario).tirer((Brigand)vilain);
-	((Cowboy)mario).tirer((Brigand)vilain);
+        System.out.println("\n\n");
 
-	((Cowboy)mario).liberer(princesse);
+        vilain.sePresenter();
+        vilain.parler("Je suis vraiment méchant.");
 
-	mario.boire();
-	
-	Barman batman = new Barman("Lopo", "Chez señor Lopo");
+        System.out.println("\n\n");
 
-	batman.sePresenter();
-	batman.parler("j'suis bourré");
+        ((Brigand) vilain).kidnapper(princesse);
+        mario.parler("PAS DE RESPECT RAMENES MA MEUF");
+        vilain.parler("MUAHAHAHAHAHAAA");
+        vilain.sePresenter();
+        mario.parler("Attends, je mange ce champignon et tu vas moins rigoler !");
 
-	princesse.sePresenter();
-	
-	
+        System.out.println("\n\n");
+
+        ((Cowboy) mario).tirer((Brigand) vilain);
+        ((Cowboy) mario).tirer((Brigand) vilain);
+
+        ((Cowboy) mario).liberer(princesse);
+
+        System.out.println("\n\n");
+
+        System.out.println("\nMario va se promener dans un bar...");
+
+        Barman batman = new Barman("Lopo", "Chez Señor Lopo");
+        mario.boire();
+
+        batman.sePresenter();
+        batman.parler("j'suis bourré");
+
+        princesse.sePresenter();
+
+        System.out.println("\n\n");
+        Cowboy clint = new Sheriff("Clint Eastwood");
+        clint.sePresenter();
+
+        /*clint.coffrer((Brigand)vilain))
+        On peut pas coffrer un Brigand car le compilateur ne sait pas à priori
+        quelle est l'instance contenue dans la référence de type Cowboy 'clint'. Il
+        suppose qu'il s'agit donc d'une instance d'un Cowboy, et un Cowboy n'a pas
+        de méthode 'coffrer'. C'est donc une erreur au moment de compiler.
+        */
+
+        
+
+
     }
 
 
